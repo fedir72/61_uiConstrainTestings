768da9ab09ab758dcf1d58fe5376b5ece7993e06
fedir72 <fedir72@ua.fm>
2020-09-17 13:15:56 +0300

    + add registration for users in firebase + refactoring code for registration code

--- constrainTest/constrainTest/Controllers/SignUpController.swift
+++ constrainTest/constrainTest/Controllers/SignUpController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
 class SignUpController: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
     
@@ -54,6 +56,7 @@ class SignUpController: UIViewController , UIImagePickerControllerDelegate , UIN
         
         configureComponents()
         setupGesture()
+        handlers()
     }
     
     //MARK: - UIImagePicker
@@ -66,13 +69,97 @@ class SignUpController: UIViewController , UIImagePickerControllerDelegate , UIN
         //MARK: - при выборе картинки для кнопки
         selectPhotoButton.layer.cornerRadius = selectPhotoButton.frame.width/2
         selectPhotoButton.layer.masksToBounds = true
-       // selectPhotoButton.layer.backgroundColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
         selectPhotoButton.layer.borderWidth = 3
         selectPhotoButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         selectPhotoButton.setImage(profileImage.withRenderingMode(.alwaysOriginal), for: .normal)
         self.dismiss(animated: true, completion: nil)
     }
     
+    //MARK: - проверка заполнения полей формы
+    
+    private func handlers() {
+        emailTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        fullNameTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        nickNameTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        passWordTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        
+        signUpButtoh.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+    }
+    
+    @objc private func handleSignUp() {
+        print("Sign Up")
+        //MARK: - save userData
+        self.handleTapDismiss()//убрать клаву
+        
+        guard let email = emailTextField.text?.lowercased() else {return}
+        guard let password = passWordTextField.text else {return}
+        guard let fullName = fullNameTextField.text else {return}
+        guard let userNick = nickNameTextField.text else {return}
+        
+        print( email , password , fullName , userNick )
+        
+         
+           //MARK: - FireBase save data
+        Auth.auth().createUser(withEmail: email, password: password) { (user, err) in
+            if let err = err {
+                print("error Firebase: " ,err.localizedDescription)
+            }
+            print("User has been created")
+            
+            //MARK: -сохранение фото в firebace
+            guard let profileImage = self.selectPhotoButton.imageView?.image else {return}
+            guard let uploaddata = profileImage.jpegData(compressionQuality: 0.3) else {return}
+            let filename = NSUUID().uuidString
+            let storageRef = Storage.storage().reference().child("profile_image").child(filename)
+            storageRef.putData(uploaddata,metadata: nil ) { (_,err) in
+                if let err = err {
+                   print( "фото не загружено: ",err.localizedDescription)
+                   return
+                }
+            print("файл загружен")
+                storageRef.downloadURL { (downloadUrl, error) in
+                    guard let profileImageUrl = downloadUrl?.absoluteString else {return}
+                    if let err = err {
+                        print("Profile image is nil", err.localizedDescription)
+                        return
+                    }
+            print("Получена ссылка на фото")
+                    
+            guard let uid = user?.user.uid else {return}
+            let dictionaryValue = ["name": fullName, "nickName": userNick,"profileImageurl": profileImageUrl]
+            let values = [uid:dictionaryValue]
+                    Firestore.firestore().collection("users").document(uid).setData(values){ (err) in
+                        if let err = err {
+                        print("Failed: ",err.localizedDescription)
+                    }
+                    print("Данные успешно сохранены")
+                     return
+                    }
+                }
+            }
+        }
+               emailTextField.text = ""
+              passWordTextField.text = ""
+              fullNameTextField.text = ""
+              nickNameTextField.text = ""
+        signUpButtoh.isEnabled = false
+        
+    }
+    
+    @objc private func formValidation() {
+      guard emailTextField.hasText,
+            fullNameTextField.hasText,
+            nickNameTextField.hasText,
+            passWordTextField.hasText,
+            imageSelected == true  else {
+            signUpButtoh.isEnabled = false
+            //signUpButtoh.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+                return
+        }
+        signUpButtoh.isEnabled = true
+        signUpButtoh.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        
+    }
     
     
     //MARK: - stack for button and labels
@@ -141,7 +228,7 @@ class SignUpController: UIViewController , UIImagePickerControllerDelegate , UIN
     private func setupGesture() {
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
     }
-    //MARK: - конец изменеий вью
+    //MARK: - конец изменений вью
     @objc fileprivate func handleTapDismiss() {
         view.endEditing(true)
     }
